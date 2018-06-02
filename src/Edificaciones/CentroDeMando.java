63808385b0ab97e9514a69d40f536c71ad015885
'Diana' <'00226217@uca.edu.sv>
2018-06-01 18:40:53 -0600

    Factory Edificaciones

---
+++ src/Edificaciones/CentroDeMando.java
@@ -0,0 +1,39 @@
+/*
+ * To change this license header, choose License Headers in Project Properties.
+ * To change this template file, choose Tools | Templates
+ * and open the template in the editor.
+ */
+package Edificaciones;
+
+/**
+ *
+ * @author Diana
+ */
+public class CentroDeMando implements Edificacion {
+
+    @Override
+    public Edificacion seConstruye() {
+        throw new UnsupportedOperationException("Not supported yet."); //To change body of generated methods, choose Tools | Templates.
+    }
+
+    @Override
+    public void vida() {
+        throw new UnsupportedOperationException("Not supported yet."); //To change body of generated methods, choose Tools | Templates.
+    }
+
+    @Override
+    public Recurso almacena() {
+        throw new UnsupportedOperationException("Not supported yet."); //To change body of generated methods, choose Tools | Templates.
+    }
+
+    @Override
+    public Vehiculo construyeVehiculo() {
+        throw new UnsupportedOperationException("Not supported yet."); //To change body of generated methods, choose Tools | Templates.
+    }
+
+    @Override
+    public Soldado entrenaSoldados() {
+        throw new UnsupportedOperationException("Not supported yet."); //To change body of generated methods, choose Tools | Templates.
+    }
+    
+}
---
+++ src/Edificaciones/Edificacion.java
@@ -0,0 +1,19 @@
+/*
+ * To change this license header, choose License Headers in Project Properties.
+ * To change this template file, choose Tools | Templates
+ * and open the template in the editor.
+ */
+package Edificaciones;
+
+/**
+ *
+ * @author Diana
+ */
+public interface Edificacion {
+    public Edificacion seConstruye();
+    public void vida();
+    public Recurso almacena();
+    public Vehiculo construyeVehiculo();
+    public Soldado entrenaSoldados();
+    
+}
