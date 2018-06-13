e88f371049e07f3f7d67e2f36dbeb0f751c9e5f2
Diana0110 <00226217@uca.edu.sv>
2018-06-13 15:49:47 -0600

    contenedores modificados

---
+++ src/Edificaciones/Contenedores/ContenedorComida.java
@@ -0,0 +1,34 @@
+/*
+ * To change this license header, choose License Headers in Project Properties.
+ * To change this template file, choose Tools | Templates
+ * and open the template in the editor.
+ */
+package Edificaciones.Contenedores;
+
+import Edificaciones.Edificacion;
+import Recursos.Recurso;
+
+/**
+ *
+ * @author Diana
+ */
+public class ContenedorComida implements Edificacion {
+
+    @Override
+    public Edificacion crearEdificacion() {
+        throw new UnsupportedOperationException("Not supported yet."); //To change body of generated methods, choose Tools | Templates.
+    }
+
+    @Override
+    public Recurso Almacena() {
+        throw new UnsupportedOperationException("Not supported yet."); //To change body of generated methods, choose Tools | Templates.
+    }
+
+    @Override
+    public void seDestruye() {
+        throw new UnsupportedOperationException("Not supported yet."); //To change body of generated methods, choose Tools | Templates.
+    }
+
+
+    
+}
